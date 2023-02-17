656c6e50e3896afb2fd709b2a03a3bc8934a4197
jaeyeon <jaeyeon423@gmail.com>
2023-02-17 09:44:49 +0900

    Spring boot 게시판 API 서버 제작 (1) - 프로젝트 생성

---
+++ board/src/test/java/farm/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package farm.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
