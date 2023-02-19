ba869c646b7bc8fc536f4e836f0b5ee46714baa9
jaeyeon <jaeyeon423@gmail.com>
2023-02-19 18:22:33 +0900

    Spring boot 게시판 API 서버 제작 (7) - 로그인 - Exception 처리 (멤버 추가)

--- board/src/main/java/farm/board/InitDB.java
+++ board/src/main/java/farm/board/InitDB.java
@@ -1,11 +1,15 @@
 package farm.board;
 
+import farm.board.domain.Member;
 import farm.board.domain.Role;
 import farm.board.domain.RoleType;
+import farm.board.exception.RoleNotFoundException;
+import farm.board.repository.member.MemberRepository;
 import farm.board.repository.role.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,17 +24,39 @@ import java.util.stream.Collectors;
 @Profile("local") // InitDB 클래스가 local 프로파일에서만 실행되도록 설정하는 역할
 public class InitDB {
     private final RoleRepository roleRepository;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @PostConstruct //객체 생성 후 초기화를 수행하는 메소드를 정의
     @Transactional
     public void initDB(){
         log.info("initialize database");
         initRole();
+        initTestAdmin();
+        initTestMember();
     }
 
     private void initRole(){
         roleRepository.saveAll( // 모든 Role_Type role에 저장
                 List.of(RoleType.values()).stream().map(roleType -> new Role(roleType)).collect(Collectors.toList())
+        );
+    }
+
+    private void initTestAdmin() {
+        memberRepository.save(
+                new Member("admin@admin.com", passwordEncoder.encode("123456a!"), "admin", "admin",
+                        List.of(roleRepository.findByRoleType(RoleType.ROLE_NORMAL).orElseThrow(RoleNotFoundException::new),
+                                roleRepository.findByRoleType(RoleType.ROLE_ADMIN).orElseThrow(RoleNotFoundException::new)))
+        );
+    }
+
+    private void initTestMember() {
+        memberRepository.saveAll(
+                List.of(
+                        new Member("member1@member.com", passwordEncoder.encode("123456a!"), "member1", "member1",
+                                List.of(roleRepository.findByRoleType(RoleType.ROLE_NORMAL).orElseThrow(RoleNotFoundException::new))),
+                        new Member("member2@member.com", passwordEncoder.encode("123456a!"), "member2", "member2",
+                                List.of(roleRepository.findByRoleType(RoleType.ROLE_NORMAL).orElseThrow(RoleNotFoundException::new))))
         );
     }
 }
